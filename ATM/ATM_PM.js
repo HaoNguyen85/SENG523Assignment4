c71d227866808e339a51a6f3db93eb6f4910efc0
Doan Hao Nguyen <haodnguyen2601@gmail.com>
2022-11-30 14:06:17 -0700

    Initializing classes
    give true to variables

--- ATM/ATM_PM.js
+++ ATM/ATM_PM.js
@@ -1,2 +1,9 @@
 import {CardScanner, Keypad, Monitor, BillStorage, BillDisburser, SystemDatabase, SystemClock} from './ATM_SM.js'
 
+const database = SystemDatabase();
+
+const ATM_cardScanner = CardScanner(true, true, true);
+const ATM_keypad = Keypad(true);
+const ATM_monitor = Monitor(true);
+const ATM_billStorage = BillStorage(true);
+const ATM_billDisburser = BillDisburser(true);
